d8df8c31e8e05002bf82f72891c3c49fecce16b7
Candela Buttigliero <cbuttigliero@hotmail.com>
2024-06-19 12:14:00 +0200

    new design in progress

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -2,8 +2,10 @@ import Tab from './Tab'
 
 const Header = ({ selectedTab, setSelectedTab, tabs }) => {
     return (
-        <div className='header-wrapper'>
-            {tabs.map((tab, index) => <Tab key={`tab__${index}`} name={tab.name} selectedTab={selectedTab} setSelectedTab={setSelectedTab} id={tab.id} />)}
+        <div className='w-100 d-flex justify-center align-center header-container'>
+            <div className='header-wrapper'>
+                {tabs.map((tab, index) => <Tab key={`tab__${index}`} name={tab.name} selectedTab={selectedTab} setSelectedTab={setSelectedTab} id={tab.id} />)}
+            </div>
         </div>
     )
 }
--- my-app/src/views/Inicio.js
+++ my-app/src/views/Inicio.js
@@ -1,11 +1,46 @@
 import Title from '../components/Title'
 import { motion } from "framer-motion";
 
+
 const Inicio = () => {
     return (
         <motion.div className='home-content'>
-            <Title title='LOBIS' subtitle='¡Buzos de egresados y más!' />
-            <motion.div className='semi-circle' initial={{
+            <div className='photos-wrapper'>
+                <div className='photo-box'>
+                    <motion.div className='photo-portrait' initial={{
+                        opacity: 0,
+                        x: 50
+                    }}
+                        whileInView={{
+                            opacity: 1,
+                            x: 0, // Slide in to its original position
+                            transition: {
+                                duration: 1 // Animation duration
+                            }
+                        }}
+                        viewport={{ once: true }}>
+                    </motion.div>
+                    <h3 className='photo-portrait-text'>Egresados</h3>
+                </div>
+                <div className='photo-box'>
+                    <motion.div className='photo-portrait photo-2' initial={{
+                        opacity: 0,
+                        x: 50
+                    }}
+                        whileInView={{
+                            opacity: 1,
+                            x: 0, // Slide in to its original position
+                            transition: {
+                                duration: 1 // Animation duration
+                            }
+                        }}
+                        viewport={{ once: true }}>
+                    </motion.div>
+                    <h3 className='photo-portrait-text business'>Business</h3>
+                </div>
+            </div>
+
+            {/* <motion.div className='semi-circle' initial={{
                 opacity: 0,
                 // if odd index card,slide from right instead of left
                 x: -50
@@ -19,7 +54,7 @@ const Inicio = () => {
                 }}
                 viewport={{ once: true }}>
                 <img src='./hoodie_girl.jpeg'></img>
-            </motion.div>
+            </motion.div> */}
         </motion.div>
     )
 }
