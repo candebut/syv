dc3e1136fad3bb0ebbb8044e444b4dad699447a4
Candela Buttigliero <cbuttigliero@hotmail.com>
2024-06-20 12:30:51 +0200

    contact added

--- my-app/src/views/Contacto.js
+++ my-app/src/views/Contacto.js
@@ -2,18 +2,19 @@ const Contacto = () => {
     return (
 
         <div className='contacto-wrapper'>
-            <div className='contacto-content'>
-                <div className='contacto-text'>
-                    <h1>Contactanos!</h1>
-                    <p className='lato'>¿Tienes alguna pregunta o deseas solicitar un presupuesto personalizado? Estamos aquí para ayudarte. Puedes ponerte en contacto con nosotros de las siguientes maneras:</p>
+            <h1>Contactanos!</h1>
+            <p className='lato'>¿Tienes alguna pregunta o deseas solicitar un presupuesto personalizado? Estamos aquí para ayudarte. Puedes ponerte en contacto con nosotros de las siguientes maneras:</p>
+            <ul className='contacto-text'>
 
-                    <p className='lato'>-
-                        <u>Correo Electrónico</u>: Envíanos un correo a <a href='mailto:contacto@syvbordados.com'>contacto@syvbordados.com</a>. Responderemos a tus consultas en un plazo breve.
-                    </p>
-                    <p className='lato'><u>Teléfono</u>: Si prefieres hablar directamente con nosotros, escribinos al <b>+54 9 11 1234 5678</b>. Estamos disponibles durante nuestro horario comercial.</p>
-                    <p className='lato'>- Visítanos en persona: Si estás en Buenos Aires, podes visitarnos en <b>Martin Coronado</b>, escribinos por whatsapp y agendamos un día. Te recibiremos con gusto y resolveremos cualquier duda que tengas.</p>
-                </div>
-            </div>
+                <ul className='lato'><u>Formulario de contacto</u>: Rellena nuestro formulario en línea con tus datos y consulta. Nos pondremos en contacto lo antes posible.</ul>
+
+                <ul className='lato'>
+
+                    <u>Correo Electrónico</u>: Envíanos un correo a <a href='mailto:lobis.egresados@gmail.com'>lobis.egresados@gmail.com</a>.
+                </ul>
+                <ul className='lato'><u>Teléfono</u>: Si prefieres hablar directamente con nosotros, escribinos al <b>+54 9 11 1234 5678</b>.</ul>
+                <ul className='lato'><u>Visítanos en persona</u>: Si estás en Buenos Aires, podes visitarnos en <b>Martin Coronado</b>, escribinos por whatsapp y agendamos un día. Te recibiremos con gusto y resolveremos cualquier duda que tengas.</ul>
+            </ul>
         </div>
 
     )
--- my-app/src/views/Presupuesto.js
+++ my-app/src/views/Presupuesto.js
@@ -1,50 +1,61 @@
 import { motion } from "framer-motion";
 const Presupuesto = () => {
     return (
-        <div className='historia-wrapper'>
-            <motion.div className='presupuesto-content' initial={{
-                opacity: 0,
-                // if odd index card,slide from right instead of left
-                x: 100,
-            }}
-                whileInView={{
-                    opacity: 1,
-                    x: 0, // Slide in to its original position
-                    transition: {
-                        duration: 1 // Animation duration
-                    }
+        <div className='presupuesto-wrapper'>
+            <div className='presupuesto'>
+                <h1>Presupuesto</h1>
+                <div className='productos-text'>
+                    <h5>En Lobis, nos esforzamos por ofrecerte un presupuesto transparente y adaptado a tus necesidades. Calculamos el valor del pedido considerando varios factores:
+                    </h5>
+                    <ul>
+                        <li>Tipo de Prenda: Elige la prenda que deseas personalizar: buzos, camisetas, camperas u otras opciones. Cada tipo de prenda tiene un costo base.</li>
+                        <li>Cantidad de Prendas: Cuantas más prendas solicites, mayor será el descuento por volumen.</li>
+                        <li>Diseño de Bordado: Nuestro bordado estándar incluye un diseño en el frente y otro en el dorso. Si tienes un logotipo o texto específico, indícanoslo para ajustar el presupuesto.</li>
+                    </ul>
+                </div>
+                <motion.div className='presupuesto-content' initial={{
+                    opacity: 0,
+                    // if odd index card,slide from right instead of left
+                    x: 100,
                 }}
-                viewport={{ once: true }}>
+                    whileInView={{
+                        opacity: 1,
+                        x: 0, // Slide in to its original position
+                        transition: {
+                            duration: 1 // Animation duration
+                        }
+                    }}
+                    viewport={{ once: true }}>
+                    <h1>Presupuesto estándar + bordado</h1>
+                    <table className="price-table">
+                        <thead></thead>
+                        <tbody>
+                            <tr>
+                                <td>remera</td>
+                                <td>desde $23.250</td>
+                            </tr>
+                            <tr>
+                                <td>buzo</td>
+                                <td>desde $36.000</td>
+                            </tr>
+                            <tr>
+                                <td>buzo canguro</td>
+                                <td>desde $38.000</td>
+                            </tr>
+                            <tr>
+                                <td>campera</td>
+                                <td>desde $40.000</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <p>
+                        Podés financiarlo en cuotas. Recuerda que estos precios son aproximados y pueden variar según la complejidad del diseño y otros detalles. Si deseas un presupuesto específico para tu pedido, no dudes en contactarnos. ¡Estamos aquí para ayudarte!
+                    </p>
 
-                <div className='empty-third'></div>
-                <table className="price-table">
-                    <thead></thead>
-                    <tbody>
-                        <tr>
-                            <td>remera</td>
-                            <td>desde $23.250</td>
-                        </tr>
-                        <tr>
-                            <td>buzo</td>
-                            <td>desde $36.000</td>
-                        </tr>
-                        <tr>
-                            <td>buzo canguro</td>
-                            <td>desde $38.000</td>
-                        </tr>
-                        <tr>
-                            <td>campera</td>
-                            <td>desde $40.000</td>
-                        </tr>
-                    </tbody>
-                </table>
-
-                <p className='patua presupuesto-text'>prenda +  bordado
-                    de 28cm x 38cm</p>
-            </motion.div>
-            <motion.div className='patua presupuesto-hint'>
-                Podés financiarlo en cuotas. Recuerda que estos precios son aproximados y pueden variar según la complejidad del diseño y otros detalles. Si deseas un presupuesto específico para tu pedido, no dudes en contactarnos. ¡Estamos aquí para ayudarte!
-            </motion.div>
+                    <p className='patua presupuesto-text'>prenda +  bordado
+                        de 28cm x 38cm</p>
+                </motion.div>
+            </div>
 
 
         </div>
